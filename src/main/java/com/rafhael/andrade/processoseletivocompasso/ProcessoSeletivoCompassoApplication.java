a2c3edec16d670cae4f7bdda71844bf7a6b5abff
Rafhael <rafhael_ab@hotmail.com>
2021-02-05 22:31:41 -0300

    Initial commit

---
+++ src/main/java/com/rafhael/andrade/processoseletivocompasso/ProcessoSeletivoCompassoApplication.java
@@ -0,0 +1,13 @@
+package com.rafhael.andrade.processoseletivocompasso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessoSeletivoCompassoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessoSeletivoCompassoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafhael/andrade/processoseletivocompasso/ProcessoSeletivoCompassoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafhael.andrade.processoseletivocompasso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessoSeletivoCompassoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
