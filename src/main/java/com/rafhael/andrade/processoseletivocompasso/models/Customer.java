9ac6ca4f5d4b70352863eb05e9dc45cc13773b19
Rafhael <rafhael_ab@hotmail.com>
2021-02-07 19:16:38 -0300

    ajust Json annotations on Customer class and setup junit in pom.xml

--- src/main/java/com/rafhael/andrade/processoseletivocompasso/models/Customer.java
+++ src/main/java/com/rafhael/andrade/processoseletivocompasso/models/Customer.java
@@ -1,6 +1,10 @@
 package com.rafhael.andrade.processoseletivocompasso.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.rafhael.andrade.processoseletivocompasso.enums.Gender;
 
 import javax.persistence.*;
@@ -25,6 +29,8 @@ public class Customer {
 
     @Column(name = "birth_date")
     @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate birthDate;
 
     @Column
